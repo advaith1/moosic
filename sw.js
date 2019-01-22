b1448da48c84010a99b911663ba20d6f25248e20
advaith1 <advaithj1@gmail.com>
2019-01-21 17:53:19 -0800

    add dbg widget, workbox support

--- sw.js
+++ sw.js
@@ -1,34 +1,8 @@
-importScripts('/cache-polyfill.js')
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
 
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('moosic-12/22/18').then(function(cache) {
-     return cache.addAll([
-       '/',
-       '/index.html',
-       '/images/moosic.png',
-       '/images/moosic2.png',
-       '/assets/css/main.css',
-       '/assets/css/font-awesome.min.css',
-       '/assets/js/main.js',
-       '/assets/js/util.js',
-       '/assets/js/breakpoints.min.js',
-       '/assets/js/browser.min.js',
-       '/assets/js/jquery.scrollex.min.js',
-       '/assets/js/jquery.min.js',
-       '/images/bg.jpg',
-       '/assets/css/images/close.svg'
-     ])
-   })
- )
-})
-
-self.addEventListener('fetch', function(event) {
-    console.log(event.request.url)
-   
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request)
-      })
-    )
-})
+workbox.routing.registerRoute(
+  /.*/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'moosic',
+  })
+);
